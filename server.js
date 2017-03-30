0f08a698f284e12606b134dab4cc72bfd068e2ba
David Li <entkenntnis@gmx.net>
2017-03-30 11:20:59 +0200

    and again

--- server.js
+++ server.js
@@ -53,7 +53,6 @@ MongoClient.connect(dbUrl, {authSource:"admin"}, function(err, database) {
   }
   db.setDB(database);
   app.listen(process.env.NODE_PORT || 3000, process.env.NODE_IP || "localhost");
-  console.log("app started")
 });
 
 // Define routes.
