d411d13637637696562bbcf9b56380f39e09bac2
David Li <entkenntnis@gmx.net>
2017-11-04 17:08:03 +0100

    moving forward, removing failed stuff

--- start.js
+++ start.js
@@ -1,7 +1,1 @@
-if (require('fs').existsSync('./LEGACY')) {
-  require('./server.js')
-} else {
-  if (process.env.antit_srcnew == "1")
-    console.log("serving new simulation")
-  require('./new-server.js')
-}
+require('./new-server.js')
