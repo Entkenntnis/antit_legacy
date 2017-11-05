14c5b796b1242f153da94f383f7b636f7aeec3d0
David Li <entkenntnis@gmx.net>
2017-11-05 08:11:59 +0100

    testing new infra

--- public/src/Simulation/Simulation_Simulation.js
+++ public/src/Simulation/Simulation_Simulation.js
@@ -1,58 +1,78 @@
 // ALL MANAGER
+(function(Optionen){
 
-var Simulation = function() {
+  var Simulation = function() {
 
-  this.cycles = 0;
-  this.playground = undefined
-  this.players = []
-  this.hills = []
-  this.sugars = []
-  this.ants = []
-  this.apples = []
-  this.bugs = []
-  this.memories = {}
-  this.bus = Minibus.create()
-  this.rng = undefined
-  this.cycles = 0
-  
-  this.playerCount = function() {
-    return Sim.players.length;
-  }
-  
-  this.init = function() {
-    Sim.rng = new Math.seedrandom("hello.")
+    this.cycles = 0;
+    this.playground = undefined
+    this.players = []
+    this.hills = []
+    this.sugars = []
+    this.ants = []
+    this.apples = []
+    this.bugs = []
+    this.memories = {}
+    this.bus = Minibus.create()
+    this.rng = undefined
+    this.cycles = 0
+    this.Opts = Optionen
     
-    var area = (1 + (API.ants.length * Optionen.SpielfeldVerhältnis)) * Optionen.SpielfeldGrundGröße;
-    var width = Math.round(Math.sqrt(area * Optionen.SpielfeldVerhältnis));
-    var height = Math.round(Math.sqrt(area / Optionen.SpielfeldVerhältnis));
-    Sim.playground = new Playground(width, height);
-  
-    for(var i = 0; i < API.ants.length; i++) {
-      Sim.players.push(new Player(i, API.ants[i]));
-      Sim.hills.push(new Hill(Sim.playground.getHillPos(), i));
+    this.playerCount = function() {
+      return Sim.players.length;
+    }
+    
+    this.init = function() {
+      Sim.rng = new Math.seedrandom("hello.")
+      var area = (1 + (API.ants.length * Optionen.SpielfeldVerhältnis)) * Optionen.SpielfeldGrundGröße;
+      var width = Math.round(Math.sqrt(area * Optionen.SpielfeldVerhältnis));
+      var height = Math.round(Math.sqrt(area / Optionen.SpielfeldVerhältnis));
+      Sim.playground = new Playground(width, height);
+    
+      for(var i = 0; i < API.ants.length; i++) {
+        Sim.players.push(new Player(i, API.ants[i]));
+        Sim.hills.push(new Hill(Sim.playground.getHillPos(), i));
+      }
+    }
+    
+    this.update = function() {
+      Sim.apples.forEach(function(apple){
+        apple.update();
+      })
+      
+      Sim.bugs.forEach(function(bug) {
+        bug.update();
+      })
+      
+      Sim.ants.forEach(function(ant) {
+        ant.update();
+      });
+      
+      Sim.hills.forEach(function(hill) {
+        hill.update();
+      });
+      
+      Sim.playground.update();
+      Sim.cycles++
     }
   }
-  
-  this.update = function() {
-    Sim.apples.forEach(function(apple){
-      apple.update();
-    })
-    
-    Sim.bugs.forEach(function(bug) {
-      bug.update();
-    })
-    
-    Sim.ants.forEach(function(ant) {
-      ant.update();
-    });
-    
-    Sim.hills.forEach(function(hill) {
-      hill.update();
-    });
-    
-    Sim.playground.update();
-    Sim.cycles++
-  }
-}
 
-var Sim = new Simulation();
+  var Sim = new Simulation()
+  
+  AntIT._rawsim = Sim
+
+  AntIT._sim = {
+    Init : Sim.init,
+    Update : Sim.update,
+    getBus : function() { return Sim.bus },
+    getCycles : function() { return Sim.cycles },
+    getPoints : function() { 
+      return Sim.players.map(function(p){return p.getPoints()}).join(",")
+    },
+  }
+
+  if (Optionen.EntwicklerModus) {
+    AntIT.Sim = Sim;
+    AntIT.Optionen = Optionen;
+  }
+
+})(AntIT._optionen)
