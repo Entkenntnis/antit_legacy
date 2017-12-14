4c7de46ee448ee4edc71d1433da87dd3febbd535
David Li <entkenntnis@gmx.net>
2017-12-14 22:47:15 +0100

    removing experiment files

--- public/src_experiment/pluginTest.js
+++
@@ -1,8 +0,0 @@
-
-
-AntIT.Bus.on('tick', function(cycle) {
-  if (AntIT.Units.Ant.length > 0 && (cycle % 40) == 0) {
-    AntIT.Units.Ant[0].move(AntIT.Board.randomPos())
-    AntIT.Units.Ant[0].turn(Math.random()*20-10)
-  }
-})
